037fb3a2e9cd2c7beb64c91ce6a04bea2b65a99a
James Dowsett <James.Dowsett@psy.lmu.de>
2022-07-15 17:43:30 +0200

    additional plots for Gamma walk 2 pipeline

--- gamma_walk_2_pipeline.py
+++ gamma_walk_2_pipeline.py
@@ -27,7 +27,9 @@ path = '/home/james/Active_projects/Gamma_walk/gamma_walk_experiment_2/Right_han
 
 electrode_names = np.load(path + '/electrode_names.npy')
 
-condition_names = ['W35', 'S35', 'W40', 'S40', 'black35', 'black40']
+condition_labels = ['W', 'S', 'black']
+
+condition_names = ['Walking', 'Standing', 'Blackout']
 
 sample_rate = 1000
 
@@ -45,21 +47,24 @@ trig_length = 4
 
 laplacian = 1
 
+if laplacian == 0:
+    montage_name = 'Cz Reference'
+elif laplacian == 1:
+    montage_name = 'Laplacian'
+
 ### Matrices to store results and SSVEPs
 
 
-all_SSVEPs = np.zeros([num_subjects,2,64,2,29]) # subject, frequency, electrode, condition, SSVEP data (29 data points is the largest SSVEP, 35 Hz)
+all_SSVEPs = np.zeros([num_subjects,2,64,3,29]) # subject, frequency, electrode, condition, SSVEP data (29 data points is the largest SSVEP, 35 Hz)
 
-SSVEP_amplitudes = np.zeros([num_subjects,2,64,2]) # subject, frequency, electrode , condition
+SSVEP_amplitudes = np.zeros([num_subjects,2,64,3]) # subject, frequency, electrode , condition
 
 SSVEP_walking_standing_correlations = np.zeros([num_subjects,2,64]) # subject, frequency, electrode
 
 SSVEP_phase_scores = np.zeros([num_subjects,6,8])
 
 
-blackout_SSVEPs = np.zeros([num_subjects,64,2,29]) # blackout  (29 data points is the largest SSVEP, 35 Hz)
 
-blackout_amplitudes = np.zeros([num_subjects,64,2])
 
 ##################################
 
@@ -71,63 +76,69 @@ for subject in range(1,num_subjects+1):
     print('Subject ' + str(subject))
     print(' ')
  
-    conditions_to_use = np.arange(0,6)
+    conditions_to_use = np.arange(0,3)
     
     if subject == 1: # subject one had no blackout condition
-        conditions_to_use = np.array([0, 1, 2, 3])
+        conditions_to_use = np.array([0, 1])
 
-    for condition in conditions_to_use:
-        
-        if condition == 0 or condition == 1 or condition == 4:
-            frequency = 35
-           
-            plt.figure(2)
-            plt.suptitle('35 Hz')
-            
-        elif condition == 2 or condition == 3 or condition == 5:
-        
-            frequency = 40
-            
-            plt.figure(3)
-            plt.suptitle('40 Hz')
-
-                  
-        condition_name = condition_names[condition]
-        
-        file_name = 'S' + str(subject) + '_' + condition_name
-
-        ## load raw data
-        if laplacian == 0:
-            all_data = np.load(path + file_name + '_all_data_interpolated.npy')
-        elif laplacian == 1:
-            all_data = np.load(path + file_name + '_all_data_interpolated_laplacian.npy')
-        
-        ## load triggers
-        triggers = np.load(path + file_name + '_all_triggers.npy')
-     
-        for electrode in range(0,64):
-            
-            electrode_name = electrode_names[electrode]
-            
-            print(' ')
-            print(electrode_name)
-            print(' ')
-            
-            data = all_data[electrode,:]
-          
     
-                ### make SSVEP
+    for frequency_count in range(0,2):
         
-            period = int(np.round(sample_rate/frequency))
+        plt.figure()
+        plt.suptitle('Subject ' + str(subject) + '  ' + str(frequencies_to_use[frequency_count]) + ' Hz ' + montage_name)
+        
+        frequency = frequencies_to_use[frequency_count]
+        
+        for condition_count in conditions_to_use:
+
+            condition_name = condition_labels[condition_count] + str(frequencies_to_use[frequency_count])
             
-            SSVEP = functions.make_SSVEPs(data, triggers, period)
+
             
+            file_name = 'S' + str(subject) + '_' + condition_name
+    
+            ## load raw data
+            if laplacian == 0:
+                all_data = np.load(path + file_name + '_all_data_interpolated.npy')
+            elif laplacian == 1:
+                all_data = np.load(path + file_name + '_all_data_interpolated_laplacian.npy')
             
-            plt.subplot(8,8,electrode+1)
+            ## load triggers
+            triggers = np.load(path + file_name + '_all_triggers.npy')
+         
+            for electrode in range(0,64):
+                
+                electrode_name = electrode_names[electrode]
+                
+                # print(' ')
+                # print(electrode_name)
+                # print(' ')
+                
+                data = all_data[electrode,:]
+              
+        
+                    ### make SSVEP
             
-            plt.title(electrode_name)
-            
-            plt.plot(SSVEP)
-            
-            plt.ylim([-0.0005, 0.0005])
-            
+                period = int(np.round(sample_rate/frequency))
+                
+                SSVEP = functions.make_SSVEPs(data, triggers, period)
+                
+                
+                # put the SSVEP into the matrix
+                all_SSVEPs[subject-1,frequency_count,electrode,condition_count,0:period] = SSVEP # subject, frequency, electrode, condition, SSVEP data (29 data points is the largest SSVEP, 35 Hz)
+    
+                # record the SSVEP amplitudes
+                SSVEP_amplitudes[subject-1,frequency_count,electrode,condition_count] = np.ptp(SSVEP) # subject, frequency, electrode , condition
+                
+                plt.subplot(8,8,electrode+1)
+                
+                plt.title(electrode_name)
+                
+                plt.plot(SSVEP)
+              
+            # set constant y axis in plots
+            max_amplitude = np.max(SSVEP_amplitudes[subject-1,frequency_count,:,:]) 
+            for electrode in range(0,64):  
+                plt.subplot(8,8,electrode+1)
+                plt.ylim([-(max_amplitude/2), (max_amplitude/2)])
+         
