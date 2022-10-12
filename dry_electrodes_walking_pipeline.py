c1f2d4338922d83f6ac7ad0a92e86b70746e6c02
James Dowsett <James.Dowsett@psy.lmu.de>
2022-10-12 17:19:04 +0200

    dry electrode artefact template subtraction

--- dry_electrodes_walking_pipeline.py
+++ dry_electrodes_walking_pipeline.py
@@ -38,252 +38,251 @@ num_subjects = 10
 period = 111
 
 ### empty matrix for the cleaned SSVEPs from the 2 dry electrodes from the walking conditions (SIGI and flicker)
+
 all_clean_segments = np.zeros([num_subjects,2,2,100000,period]) # 10 subjects, 2 dry electrodes, 2 walking conditions, segment number, segment data
 
-for subject in (0,1,2,4,5,6,7,8,9):
+# all_clean_segments = np.load(path + 'all_clean_segments.npy')
+
+# for subject in (0,1,2,4,5,6,7,8,9):
     
     
-#subject = 1
+subject = 1
     
-    print('Subject ' + str(subject))
+print('Subject ' + str(subject))
+
+# plt.figure()
+# plt.suptitle('Subject ' + str(subject))
+
+plot_count = 0
+
+electrode_count = 0
+
+for electrode in (4,5):  ### range(4,8): ##
     
-    # plt.figure()
-    # plt.suptitle('Subject ' + str(subject))
+  
+    # plot_count += 1
+    # plt.subplot(2,2,plot_count)
+    # plt.title(electrode_names[electrode])
+
+# load data, all conditions should be one data file
+
+
+    file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_chan_' + str(electrode) + '_data.npy'
     
-    #plot_count = 0
+    data = np.load(file_name)
+
+
+    ### plot raw data
+    # plt.figure(1)
+    # data = data-data.mean()
+    # plt.plot(data, label = electrode_names[electrode])
+    # plt.legend()
     
-    electrode_count = 0
     
-    for electrode in (4,5):
+    # get Accelerometer data
+    
+    # accelerometer_x_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_1_data.npy'    
+    # accelerometer_x_data = np.load(accelerometer_x_file_name)
         
-        # plt.figure(2)
-        # plot_count += 1
-        # plt.subplot(2,2,plot_count)
-        # plt.title(electrode_names[electrode])
-    
-    # load data, all conditions should be one data file
-    
-    
-        file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_chan_' + str(electrode) + '_data.npy'
+    # accelerometer_y_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_2_data.npy'  
+    # accelerometer_y_data = np.load(accelerometer_y_file_name)
         
-        data = np.load(file_name)
-    
-    
-        ### plot raw data
-        # plt.figure(1)
-        # data = data-data.mean()
-        # plt.plot(data, label = electrode_names[electrode])
-        # plt.legend()
-        
-        
-        # get Accelerometer data
-        
-        # accelerometer_x_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_1_data.npy'    
-        # accelerometer_x_data = np.load(accelerometer_x_file_name)
-            
-        # accelerometer_y_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_2_data.npy'  
-        # accelerometer_y_data = np.load(accelerometer_y_file_name)
-            
-        # accelerometer_z_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_3_data.npy'  
-        # accelerometer_z_data = np.load(accelerometer_z_file_name)
-          
-        # # plot_scale = 1000
-        # # plt.plot(accelerometer_x_data*plot_scale)
-        # # plt.plot(accelerometer_y_data*plot_scale)
-        # # plt.plot(accelerometer_z_data*plot_scale)
-        
-        # # get gyroscope data
-    
-        # gyroscope_x_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_4_data.npy'     
-        # gyroscope_x_data = np.load(gyroscope_x_file_name)
-          
-        # gyroscope_y_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_6_data.npy'     
-        # gyroscope_y_data = np.load(gyroscope_y_file_name)
-              
-        # gyroscope_z_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_5_data.npy'  
-        # gyroscope_z_data = np.load(gyroscope_z_file_name)    
-            
-    
-     
-      #  plt.plot(gyroscope_x_data)
-        # plt.plot(gyroscope_y_data)
-        # plt.plot(gyroscope_z_data)
-        
-        walking_condition_count = 0
-        
-        for condition in (1,3): #range(0,5):
-            
-            ## load triggers for condition
-            
-            condition_name = condition_names[condition]
-            
-            print(condition_name)
-            
-            trigger_file_name = path + 'subject_' + str(subject+1) + '/Subject_' + str(subject+1) + '_all_triggers_' + condition_name + '.npy'
-            
-            triggers = np.load(trigger_file_name)
-          
-            ## make a time series of the triggers to visualize
-            # plt.figure(1)
-            # trigger_time_series = np.zeros([len(data)],)
-             
-            # for trigger in triggers:
-            #     trigger_time_series[trigger] = 2000
-    
-            # plt.plot(trigger_time_series)
-                
-                
-            ## make SSVEP the normal way
-            
-          #  SSVEP = functions.make_SSVEPs(data, triggers, period) # 
-        
-            # plt.figure(2)
-            # plt.plot(SSVEP, label = condition_names[condition])
-        
-        
-            ################  motion artefact removal  #################
-            
-          #  clean_segment_matrix = np.zeros([len(triggers),period])
-            
-            first_trigger = triggers[0]
-            last_trigger = triggers[-1]
-    
-            
-            
-            # timer
-            start_time = timer() # start a timer to keep see how long the artefact removal is taking
-            time_stamps = np.zeros([len(triggers),])
-            
-            trig_count = 0
-            for trigger in triggers:
-                
-                print(' ')
-                print('Subject ' + str(subject) + ' ' + electrode_names[electrode] + '  ' + condition_name + '   Trigger ' + str(trig_count) + ' of ' + str(len(triggers)))
-             
-                 ##### display estimated time remaining ##########################################
-                   
-                print('  ')
-                   
+    # accelerometer_z_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_3_data.npy'  
+    # accelerometer_z_data = np.load(accelerometer_z_file_name)
       
-                elapsed_time_in_seconds = timer() - start_time
-                   
-                print('Time elapsed in seconds = ' + str(np.round(elapsed_time_in_seconds,1)))
-                   
-                time_stamps[trig_count] = timer() - start_time
-                   
-                if trig_count > 2:
-                   
-                   time_per_trigger = (np.diff(time_stamps[1:trig_count])).mean() 
-                   
-                   time_remaining_in_seconds = (len(triggers) - trig_count) * time_per_trigger
-                   
-                   time_remaining = timedelta(seconds=time_remaining_in_seconds)
-                   
-                   print('Average time per trigger = ' + str(np.round(time_per_trigger,2)))
-                   
-                   print('Estimated time remaining = ' + str(time_remaining))
-                #################################################################################
-                            
-                
-                
-                
-                # get the segment to be cleaned
-                segment = data[trigger:trigger+period]
-                
-               # segment = segment - segment.mean()
-                
-                #segment_range = np.ptp(segment)
-                
-                # check the entire trial for segments which correlate well and are a similar amplitude
-                
-                # cutoff value above which to include the segment in the template
-                corr_cutoff = 0.95
-                # if the peak to peak amplitude of the difference between the segment to be cleaned and the template segment is greater than this value, ignore
-                difference_cutoff = 200 
-                
-                max_num_template_segments = 50
-                
-                segment_locs = []
-                
-                # first look for segments before the trigger,
-                if (trigger-30000) < first_trigger:  # if the first trigger was more than 30 seconds ago start from the first trigger
-                    k = first_trigger 
-                else:
-                    k = trigger-30000 # if the first trigger was less than 30 seconds ago, start from 30 seconds ago
-                
-                while k < (trigger-200): # look up to 200 ms before the trigger time so we don't include the segment itself
-                    
-                    temp_segment = data[k:k+period]
-                    if np.ptp(segment-temp_segment) < difference_cutoff:
-                        if np.corrcoef(segment,temp_segment)[0,1] > corr_cutoff:
-                           segment_locs.append(k)
-                           
-                    if len(segment_locs) > max_num_template_segments:
-                        break
-                    
-                    k += 1
-                    
-                
-                k = trigger + 200
-                while k < (trigger+30000): # next look for segments after the trigger until 30 seconds after the trigger, skip the first200 ms after the trigger
-                    
-                    temp_segment = data[k:k+period]
-                    if np.ptp(segment-temp_segment) < difference_cutoff:
-                        if np.corrcoef(segment,temp_segment)[0,1] > corr_cutoff:
-                           segment_locs.append(k)
-                           
-                    if len(segment_locs) > max_num_template_segments:
-                        break
-                    
-                    k += 1               
-                
-                
-              
-                print('\n' + str(len(segment_locs)) + ' template segments')
-                
-                
-                if len(segment_locs) > 10: # only subtract the template if there are at least 10 segments contributing to the template
-                    
-                    temp_segment_matrix = np.zeros([len(segment_locs),period])
-                    seg_count = 0
-                    for seg_time in segment_locs:
-                       
-                        temp_segment = data[seg_time:seg_time+period]
-                       # temp_segment = temp_segment - temp_segment.mean()
-                        temp_segment_matrix[seg_count,:] = temp_segment
-                        seg_count += 1
-                        #plt.plot(temp_segment,'r')
-                   
-                     
-                    average_temp_segments = temp_segment_matrix[0:seg_count,:].mean(axis = 0)    
+    # # plot_scale = 1000
+    # # plt.plot(accelerometer_x_data*plot_scale)
+    # # plt.plot(accelerometer_y_data*plot_scale)
+    # # plt.plot(accelerometer_z_data*plot_scale)
     
-                    clean_segment = segment - average_temp_segments # clean the segment by subtracting the template
+    # # get gyroscope data
+
+    # gyroscope_x_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_4_data.npy'     
+    # gyroscope_x_data = np.load(gyroscope_x_file_name)
+      
+    # gyroscope_y_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_6_data.npy'     
+    # gyroscope_y_data = np.load(gyroscope_y_file_name)
+          
+    # gyroscope_z_file_name = path + 'subject_' + str(subject+1) + '/subject_' + str(subject+1) + '_acc_chan_5_data.npy'  
+    # gyroscope_z_data = np.load(gyroscope_z_file_name)    
+        
+
+ 
+  #  plt.plot(gyroscope_x_data)
+    # plt.plot(gyroscope_y_data)
+    # plt.plot(gyroscope_z_data)
     
-                   # clean_segment_matrix[trig_count,:] = clean_segment
-                    all_clean_segments[subject,electrode_count,walking_condition_count,trig_count,:] = clean_segment
+    
+    
+    
+    walking_condition_count = 0
+    
+    for condition in (1,3):  #range(0,5):  ## 
+        
+        ## load triggers for condition
+        
+        condition_name = condition_names[condition]
+        
+        print(condition_name)
+        
+        trigger_file_name = path + 'subject_' + str(subject+1) + '/Subject_' + str(subject+1) + '_all_triggers_' + condition_name + '.npy'
+        
+        triggers = np.load(trigger_file_name)
+      
+        ## make a time series of the triggers to visualize
+        # plt.figure(1)
+        # trigger_time_series = np.zeros([len(data)],)
+         
+        # for trigger in triggers:
+        #     trigger_time_series[trigger] = 2000
+
+        # plt.plot(trigger_time_series)
+            
+            
+        ## make SSVEP the normal way
+        
+        SSVEP = functions.make_SSVEPs(data, triggers, period) # 
+    
+        
+        
+    
+    
+        #### make SSVEP from the pre-cleaned segments
+        
+        # if (condition == 1 or condition == 3) and (electrode == 4 or electrode == 5):
+        
+        #     if condition == 1: 
+        #         walking_condition_count = 0  
+        #     elif condition == 3:
+        #         walking_condition_count = 1
+            
+        #     if electrode == 4:
+        #         electrode_count = 0
+        #     elif electrode == 5:
+        #         electrode_count = 1
+            
+            
+        #     trig_count = 0
+            
+        #     segment = all_clean_segments[subject,electrode_count,walking_condition_count,trig_count,:] 
+            
+        #     while np.ptp(segment) > 0:
+                
+        #         trig_count += 1
+                
+        #         segment = all_clean_segments[subject,electrode_count,walking_condition_count,trig_count,:] 
+                
+                
                     
-                else:
-                    all_clean_segments[subject,electrode_count,walking_condition_count,trig_count,:] = segment
-                   # clean_segment_matrix[trig_count,:] = segment # if there are not enough template segments, just use the original segment
+        #     all_segments = all_clean_segments[subject,electrode_count,walking_condition_count,0:trig_count,:] 
     
+        #     SSVEP = all_segments.mean(axis=0) # average to make SSVEP
+            
+        #     SSVEP = SSVEP - SSVEP.mean() # baseline correct
+        
+        
+        # ### plot SSVEP
+        
+        # plt.plot(SSVEP, label = condition_names[condition])
+        
+        
+        
+    
+        ################  motion artefact removal  #################
+        
+        
+        first_trigger = triggers[0]
+        last_trigger = triggers[-1]
+
+        length_of_artefact_segment = period * 5 # length of segments of data from which to remove the motion artefact from
+
+        # timer
+        start_time = timer() # start a timer to keep see how long the artefact removal is taking
+        time_stamps = np.zeros([len(triggers),])
+        
+        
+        trig_count = 0
+        
+
+        while trig_count < (len(triggers)-10): # loop untill 10 triggers before the end, because flicker is 9 Hz and artefact removal uses one second segments
+            
+            
+            print(' ')
+            print('Subject ' + str(subject) + ' ' + electrode_names[electrode] + '  ' + condition_name + '   Trigger ' + str(trig_count) + ' of ' + str(len(triggers)))
+         
+              ##### display estimated time remaining ##########################################
+               
+            print('  ')
+               
+  
+            elapsed_time_in_seconds = timer() - start_time
+               
+            print('Time elapsed in seconds = ' + str(np.round(elapsed_time_in_seconds,1)))
+               
+            
+               
+            if trig_count > 2:
+               
+                time_per_trigger = elapsed_time_in_seconds/trig_count
+               
+                time_remaining_in_seconds = (len(triggers) - trig_count) * time_per_trigger
+               
+                time_remaining = timedelta(seconds=time_remaining_in_seconds)
+               
+                print('Average time per trigger = ' + str(np.round(time_per_trigger,2)))
+               
+                print('Estimated time remaining = ' + str(time_remaining))
+            #################################################################################
+                   
+            
+            k = triggers[trig_count]
+
+            # get artefact segment 
+            artefact_segment = data[k:k+length_of_artefact_segment]
+            
+            artefact_segment_start_time = triggers[trig_count]
+            
+            if np.ptp(artefact_segment) < 200: # if motion artefact is not sufficently large, ignore and just use the original data
+                
+                cleaned_artefact_segment = artefact_segment
+                
+            else:
+                
+               cleaned_artefact_segment = np.zeros([length_of_artefact_segment,])
+               
+               for t in range(0,length_of_artefact_segment-10):
+                   
+                   value_min = min(artefact_segment[t:t+10])
+                   value_max = max(artefact_segment[t:t+10])
+                    
+                
+            ## put the 9 segments from the one second segment into the clean segment matrix
+            trigger_time = artefact_segment_start_time
+            while trigger_time <= (artefact_segment_start_time + length_of_artefact_segment - period):
+                
+                segment = cleaned_artefact_segment[trigger_time - artefact_segment_start_time:trigger_time - artefact_segment_start_time+period]
+                
+                all_clean_segments[subject,electrode_count,walking_condition_count,trig_count,:] = segment
+                
                 trig_count += 1
-                 
-                ## plots of individual segments
-                # plt.figure() 
-                # plt.plot(average_temp_segments,'k')
-                # plt.plot(segment,'b')
-                # plt.title(len(segment_locs))
-                # plt.plot(clean_segment,'g')
                 
-            walking_condition_count += 1   
-            # clean_SSVEP = clean_segment_matrix[0:trig_count,:].mean(axis=0)
-            # clean_SSVEP = clean_SSVEP - clean_SSVEP.mean()
+                trigger_time = triggers[trig_count] 
+                
+                
             
-          
             
-        electrode_count += 1
+         
+        walking_condition_count += 1   
+
+        
+    electrode_count += 1
           
+    
+    
+    
           
 #plt.legend()
 
-np.save(path + 'all_clean_segments',all_clean_segments)
+#np.save(path + 'all_clean_segments',all_clean_segments)
+
+
--- simulated_data_adaptive_template_subtraction.py
+++ simulated_data_adaptive_template_subtraction.py
@@ -14,7 +14,9 @@ import matplotlib.pyplot as plt
 
 import random
 
-trial_time = 2 # total time in seconds
+from flicker_analysis_package import functions
+
+trial_time = 30 # total time in seconds
 
 period = 111
 
@@ -44,22 +46,161 @@ simulated_SSVEP_data[0:number_of_flickers*period] = np.tile(one_cycle_SSVEP,numb
 
 
 
-simulated_triggers = np.arange(0, len(simulated_SSVEP_data)-period, period) # make triggers, stop one period length before the end
+triggers = np.arange(0, len(simulated_SSVEP_data)-period, period) # make triggers, stop one period length before the end
 
+
+## load one segment of typical walking artefact
+artefact_segment = np.load('example_artefact_segment.npy')
+
+artefact_segment = artefact_segment - artefact_segment.mean() # baseline correct
+
+number_of_walking_cycles =  int(len(simulated_SSVEP_data)/len(artefact_segment))  # int number of times the walking artefact will fit into the data
+walking_artefact_data = np.zeros([trial_time * sample_rate])
+walking_artefact_data[0:number_of_walking_cycles*len(artefact_segment)] = np.tile(artefact_segment,number_of_walking_cycles)
+
+
+## combine all data
+
+data = simulated_SSVEP_data + walking_artefact_data
 
 # plot raw data
 
 trigger_time_series = np.zeros([len(simulated_SSVEP_data)],)
-for trigger in simulated_triggers:
+for trigger in triggers:
     trigger_time_series[trigger] = 1
 
+
+plt.figure()
 
 # plt.plot(trigger_time_series)
     
 # plt.plot(simulated_SSVEP_data)
 
+# plt.plot(walking_artefact_data)
 
-##
+# plt.plot(data)
+
+
+## make SSVEP
+
+SSVEP = functions.make_SSVEPs(data,triggers,period)
+
+
+plt.plot(one_cycle_SSVEP)
+
+plt.plot(SSVEP)
+
+
+
+
+########### random walk template construction #################
+        
+# random_walk_template = np.zeros([len(data)],)
+
+# k = 0
+
+# value = 0
+
+# while k < len(data):
+    
+#     random_walk_template[k] = data[k] + ((random.random() * 10) - 5)
+    
+#     k+=1
+
+
+
+####################  adaptive template subtraction with random segments from the rest of the data  ######################
+
+
+
+
+length_artefact_segment = period * 5
+
+num_templates = 20
+
+clean_segment_matrix = np.zeros([len(triggers),period])
+
+k = 0
+trig_count = 0
+
+while trig_count < len(triggers) - 10:
+    
+    print('Trigger ' + str(trig_count) + ' of ' + str(len(triggers)))
+    
+    artefact_segment_start_time = triggers[trig_count]
+    
+    artefact_segment = data[artefact_segment_start_time:artefact_segment_start_time+length_artefact_segment]
+
+    template_matrix = np.zeros([num_templates,length_artefact_segment])
+        
+
+    template_count = 0
+    k = period
+    
+    while (template_count < num_templates) and (k < len(data)-length_artefact_segment):
+ 
+        temp_template = data[k:k+length_artefact_segment]
+
+        if np.corrcoef(artefact_segment,temp_template)[0,1] > 0.9:
+           
+            
+            ## check up to 20 data points ahead and behind for the best correlation
+            temp_corr_scores = np.zeros([40,])
+            
+            count = 0
+            for t in range(-20,20):
+                temp_template = data[k+t:k+t+length_artefact_segment]
+              #  plt.plot(temp_template,'c')
+                temp_corr_scores[count] = np.corrcoef(artefact_segment,temp_template)[0,1]
+                count += 1
+                
+            best_time_index = np.argmax(temp_corr_scores) - 20
+            
+            best_template = data[k+best_time_index:k+best_time_index+length_artefact_segment]
+            
+            template_matrix[template_count,:] = best_template
+            template_count += 1
+        
+            best_template = best_template - best_template.mean()
+            
+          #  plt.plot(best_template,'r')
+            
+            k = k + 500
+            
+        k += 1
+
+    average_template = template_matrix[0:template_count,:].mean(axis=0)
+
+  #  plt.plot(artefact_segment,'r')
+
+    cleaned_artefact_segment = artefact_segment - average_template
+    
+ #   plt.plot(cleaned_artefact_segment,'g')
+
+
+    ## put the 9 segments from the one second segment into the clean segment matrix
+    trigger_time = artefact_segment_start_time
+    while trigger_time <= (artefact_segment_start_time + length_artefact_segment - period):
+        
+        segment = cleaned_artefact_segment[trigger_time - artefact_segment_start_time:trigger_time - artefact_segment_start_time+period]
+        
+        clean_segment_matrix[trig_count,:] = segment
+        
+        trig_count += 1
+        
+        trigger_time = triggers[trig_count] 
+        
+
+clean_SSVEP = clean_segment_matrix[0:trig_count,:].mean(axis=0) # average segments to make the SSVEP
+
+clean_SSVEP = clean_SSVEP - clean_SSVEP.mean() # baseline correct
+
+plt.plot(clean_SSVEP,'g')
+
+
+
+
+##################### test with just subtracting random segments
 
 # numbers_of_segs_to_test = np.arange(10,100,10)
 
@@ -70,38 +211,38 @@ for trigger in simulated_triggers:
 
 # for number_of_segs in numbers_of_segs_to_test:
 
-number_of_segs = 2   
+# number_of_segs = 2   
 
-segment_matrix = np.zeros([len(simulated_triggers),period])
-seg_count = 0
+# segment_matrix = np.zeros([len(simulated_triggers),period])
+# seg_count = 0
 
 
-for trigger in simulated_triggers:
+# for trigger in simulated_triggers:
     
-    segment = simulated_SSVEP_data[trigger:trigger+period]
+#     segment = simulated_SSVEP_data[trigger:trigger+period]
     
-    random_segment_matrix = np.zeros([number_of_segs,period])
-    for random_seg_count in range(0,number_of_segs):
-        random_position = random.randint(0, len(simulated_SSVEP_data)-period) # pick a randon point in the data
-        random_segment = simulated_SSVEP_data[random_position:random_position+period]
-        random_segment_matrix[random_seg_count,:] = random_segment
+#     random_segment_matrix = np.zeros([number_of_segs,period])
+#     for random_seg_count in range(0,number_of_segs):
+#         random_position = random.randint(0, len(simulated_SSVEP_data)-period) # pick a randon point in the data
+#         random_segment = simulated_SSVEP_data[random_position:random_position+period]
+#         random_segment_matrix[random_seg_count,:] = random_segment
     
-    averaged_random_segs = random_segment_matrix.mean(axis=0)
+#     averaged_random_segs = random_segment_matrix.mean(axis=0)
     
-    segment = segment - averaged_random_segs
+#     segment = segment - averaged_random_segs
     
-    segment_matrix[seg_count,:] = segment
-    seg_count += 1
+#     segment_matrix[seg_count,:] = segment
+#     seg_count += 1
     
-SSVEP = segment_matrix.mean(axis=0)
+# SSVEP = segment_matrix.mean(axis=0)
 
-# differences_in_amplitude[count] = np.ptp(SSVEP) - np.ptp(one_cycle_SSVEP)
+# # differences_in_amplitude[count] = np.ptp(SSVEP) - np.ptp(one_cycle_SSVEP)
 
-# count += 1
+# # count += 1
 
-plt.plot(SSVEP)
+# plt.plot(SSVEP)
 
-plt.plot(one_cycle_SSVEP)
+# plt.plot(one_cycle_SSVEP)
     
         
 # plt.plot(numbers_of_segs_to_test,differences_in_amplitude)   
