6ca29aa7a0745ce52d54e8aeface980fe1b7355b
James Dowsett <James.Dowsett@psy.lmu.de>
2022-10-31 14:54:25 +0100

    added walking speeds alpha pipeline

--- walking_speeds_alpha_pipeline.py
+++ walking_speeds_alpha_pipeline.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 30 17:22:13 2022
+Created on Mon Oct 31 13:58:52 2022
 
 @author: James Dowsett
 """
@@ -20,10 +20,9 @@ import scipy.stats
 from random import choice
 import statistics
 
+### information about the experiment: walking speeds Alpha
 
-### information about the experiment: walking speeds Gamma
-
-path = '/home/james/Active_projects/walking_speeds/subject_data/'
+path = '/home/james/Active_projects/walking_speeds/walking_speeds_alpha/' # put path here
 
 electrode_names = ('O1', 'O2', 'P3', 'P4', 'P7', 'P8', 'HEOG', 'VEOG', 'x_dir', 'y_dir', 'z_dir')
 
@@ -33,16 +32,17 @@ location_names = ('hall', 'lobby')
 
 sample_rate = 1000
 
-num_subjects = 25
+num_subjects = 1
 
-period = 25 # period of flicker in ms
+period = 100 # period of flicker in ms
 
 length = 1 # length of FFT in seconds
 
 ## trigger times for linear interpolation
-trig_1_time = -1
-trig_2_time = 11
-trig_length = 4
+trig_1_time = -3
+trig_2_time = 48
+trig_length = 6
+
 
 
 #######################################
@@ -51,7 +51,7 @@ trig_length = 4
 ### Matrices to store results and SSVEPs
 
 
-all_SSVEPs = np.zeros([num_subjects,2,8,4,25]) # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
+all_SSVEPs = np.zeros([num_subjects,2,8,4,period]) # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
 
 SSVEP_amplitudes = np.zeros([num_subjects,2,8,4]) # subject, location, electrode , condition
 
@@ -68,129 +68,130 @@ all_evoked_FFTs = np.zeros([num_subjects,2,8,4,(length * sample_rate)]) # subjec
 
 ##################################
 
+## loop subject
+## loop location
+
+subject = 1
+
+location = 0
 
 
-for subject in range(1,26):
    
-    print('  ')
-    print('Subject ' + str(subject))
+print('Location = ' + location_names[location])
+ 
+## load and sort triggers
+   
+walking_triggers_file_name = 'S' + str(subject) + '_' + location_names[location] + '_walking_all_triggers.npy'
+
+all_triggers = np.load(path + walking_triggers_file_name)
+
+diff_triggers = np.diff(all_triggers)
+
+# empty lists to put the triggers into for each walking speed
+slow_triggers = []
+mid_triggers = []
+fast_triggers = []
+
+speed = 0 # set to zero to ignore any triggers before the pace markers
+
+for k in range(0,len(diff_triggers)):
+               
+    diff_trig = diff_triggers[k]
+    # if the triggers are a pace marker, set the walking speed
+    if np.abs(diff_trig - 1000) <= 3:
+        speed = 1
+    elif np.abs(diff_trig - 665) <= 3:
+        speed = 2
+    elif np.abs(diff_trig - 500) <= 3:
+        speed = 3
+        
+    if np.abs(diff_trig - period) <= 2: # if triggers are flicker, put into correct list
+        
+        if speed == 1:
+            slow_triggers.append(all_triggers[k])
+        elif speed == 2:
+            mid_triggers.append(all_triggers[k])    
+        elif speed == 3:
+            fast_triggers.append(all_triggers[k])    
+            
+print(str(len(slow_triggers)) + ' slow triggers')
+print(str(len(mid_triggers)) + ' mid triggers')
+print(str(len(fast_triggers)) + ' fast triggers')
+     
+# convert to numpy arrays
+slow_triggers = np.array(slow_triggers, dtype=int)
+mid_triggers = np.array(mid_triggers, dtype=int)
+fast_triggers = np.array(fast_triggers, dtype=int)
+
+standing_triggers_file_name = 'S' + str(subject) + '_' + location_names[location] + '_standing_all_triggers.npy' 
+standing_triggers = np.load(path + standing_triggers_file_name)
+
+
+for electrode in range(0,8):
+
+
+    electrode_name = electrode_names[electrode]
+    
     print(' ')
- 
-    for location in range(0,2):    
+    print(electrode_name)
+    print(' ')
+
+
+    for condition in range(0,4):
         
-        print('Location = ' + location_names[location])
- 
-    ## load and sort triggers
-   
-        walking_triggers_file_name = 'S' + str(subject) + '_' + location_names[location] + '_walking_all_triggers.npy'
+        print('\n' + condition_names[condition] + '\n')
     
-        all_triggers = np.load(path + walking_triggers_file_name)
+        ## load raw data
+        if condition == 0:
+            data_file_name = 'S' + str(subject) + '_' + location_names[location] + '_standing_chan_' + str(electrode) + '_data.npy'     
+        elif condition > 0:
+            data_file_name = 'S' + str(subject) + '_' + location_names[location] + '_walking_chan_' + str(electrode) + '_data.npy'
         
-        diff_triggers = np.diff(all_triggers)
-        
-        # empty lists to put the triggers into for each walking speed
-        slow_triggers = []
-        mid_triggers = []
-        fast_triggers = []
-        
-        speed = 0 # set to zero to ignore any triggers before the pace markers
-        
-        for k in range(0,len(diff_triggers)):
-                       
-            diff_trig = diff_triggers[k]
-            # if the triggers are a pace marker, set the walking speed
-            if np.abs(diff_trig - 1000) <= 3:
-                speed = 1
-            elif np.abs(diff_trig - 665) <= 3:
-                speed = 2
-            elif np.abs(diff_trig - 500) <= 3:
-                speed = 3
-                
-            if np.abs(diff_trig - 25) <= 2: # if triggers are flicker, put into correct list
-                
-                if speed == 1:
-                    slow_triggers.append(all_triggers[k])
-                elif speed == 2:
-                    mid_triggers.append(all_triggers[k])    
-                elif speed == 3:
-                    fast_triggers.append(all_triggers[k])    
-                    
-        print(str(len(slow_triggers)) + ' slow triggers')
-        print(str(len(mid_triggers)) + ' mid triggers')
-        print(str(len(fast_triggers)) + ' fast triggers')
-             
-        # convert to numpy arrays
-        slow_triggers = np.array(slow_triggers, dtype=int)
-        mid_triggers = np.array(mid_triggers, dtype=int)
-        fast_triggers = np.array(fast_triggers, dtype=int)
-    
-        standing_triggers_file_name = 'S' + str(subject) + '_' + location_names[location] + '_standing_all_triggers.npy' 
-        standing_triggers = np.load(path + standing_triggers_file_name)
-    
-        for electrode in range(0,8):
-   
-        
-            electrode_name = electrode_names[electrode]
+        raw_data = np.load(path + data_file_name)         
+
+
+        if condition == 0:
+            triggers = standing_triggers
+        elif condition == 1:
+            triggers = slow_triggers
+        elif condition == 2:
+            triggers = mid_triggers
+        elif condition == 3:
+            triggers = fast_triggers
             
-            print(' ')
-            print(electrode_name)
-            print(' ')
+        
+        ### linear interpolation
+        data_linear_interpolation = functions.linear_interpolation(raw_data, triggers, trig_1_time, trig_2_time, trig_length)
+        
 
+        # make SSVEP
+        SSVEP = functions.make_SSVEPs(data_linear_interpolation, triggers, period) # SIGI was always 10 Hz, length = 100
 
-            for condition in range(0,4):
-                
-                print('\n' + condition_names[condition] + '\n')
-            
-                ## load raw data
-                if condition == 0:
-                    data_file_name = 'S' + str(subject) + '_' + location_names[location] + '_standing_chan_' + str(electrode) + '_data.npy'     
-                elif condition > 0:
-                    data_file_name = 'S' + str(subject) + '_' + location_names[location] + '_walking_chan_' + str(electrode) + '_data.npy'
-                
-                raw_data = np.load(path + data_file_name)         
+         # save SSVEP
+        all_SSVEPs[subject-1, location, electrode, condition,:] = SSVEP  # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
+
+        # get absolute self phase shift
+        phase_shift = functions.phase_shift_SSVEPs_split(data_linear_interpolation, triggers, period)
+        
+        all_mean_self_absolute_phase_shifts[subject-1, location, electrode, condition] = phase_shift 
+
+        # get self split amplitude difference
+        split_amplitude_difference = functions.SSVEP_split_amplitude_difference(data_linear_interpolation, triggers, period)
+
+        self_split_amplitude_differences[subject-1, location, electrode, condition] = split_amplitude_difference
+
+        # get self correlation
+        self_correlation = functions.compare_SSVEPs_split(data_linear_interpolation, triggers, period)
+
+        self_split_correlation[subject-1, location, electrode, condition] = self_correlation 
+
+        ## get Evoked FFT
 
         
-                if condition == 0:
-                    triggers = standing_triggers
-                elif condition == 1:
-                    triggers = slow_triggers
-                elif condition == 2:
-                    triggers = mid_triggers
-                elif condition == 3:
-                    triggers = fast_triggers
-                    
-                
-                ### linear interpolation
-                data_linear_interpolation = functions.linear_interpolation(raw_data, triggers, trig_1_time, trig_2_time, trig_length)
-                
-    
-                # make SSVEP
-                SSVEP = functions.make_SSVEPs(data_linear_interpolation, triggers, 25) # SIGI was always 40 Hz, length = 25
+        evoked_FFT = functions.evoked_fft(data_linear_interpolation, triggers, length, sample_rate)
+        
+        all_evoked_FFTs[subject-1, location, electrode, condition,:] = evoked_FFT # subject, location, electrode, condition, FFT data 
 
-                 # save SSVEP
-                all_SSVEPs[subject-1, location, electrode, condition,:] = SSVEP  # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
-
-                # get absolute self phase shift
-                phase_shift = functions.phase_shift_SSVEPs_split(data_linear_interpolation, triggers, period)
-                
-                all_mean_self_absolute_phase_shifts[subject-1, location, electrode, condition] = phase_shift 
-
-                # get self split amplitude difference
-                split_amplitude_difference = functions.SSVEP_split_amplitude_difference(data_linear_interpolation, triggers, period)
-
-                self_split_amplitude_differences[subject-1, location, electrode, condition] = split_amplitude_difference
-
-                # get self correlation
-                self_correlation = functions.compare_SSVEPs_split(data_linear_interpolation, triggers, period)
-
-                self_split_correlation[subject-1, location, electrode, condition] = self_correlation 
-
-                ## get Evoked FFT
-
-                
-                evoked_FFT = functions.evoked_fft(data_linear_interpolation, triggers, length, sample_rate)
-                
-                all_evoked_FFTs[subject-1, location, electrode, condition,:] = evoked_FFT # subject, location, electrode, condition, FFT data 
 
 
 
@@ -230,11 +231,12 @@ self_split_correlation = np.load(path + 'self_split_correlation.npy')
 all_evoked_FFTs = np.load(path + 'all_evoked_FFTs.npy')
 
 
-#############
+
+
 
 ### plot raw SSVEPs per electrode to check data quality
 
-electrode_name = 'P4'
+electrode_name = 'O1'
 
 electrode = electrode_names.index(electrode_name)
 
@@ -244,9 +246,9 @@ for location in range(0,2):
     plt.suptitle(location_names[location] + ' ' + electrode_name)
 
 
-    for subject in range(0,25):
+    for subject in range(0,num_subjects):
         
-        plt.subplot(5,5,subject+1)
+        plt.subplot(2,5,subject+1)
 
         plt.title(subject+1)
 
@@ -258,7 +260,6 @@ for location in range(0,2):
 
 
     plt.legend()
-
 
 
 
@@ -398,86 +399,6 @@ plt.ylabel('Absolute Phase Shift (degrees)')
 
 
 
-## compare standing-walking mid phase shifts: relative to EOG
-
-print('\nCompare phase shift in each electrode to the phase shift in the VEOG\n')
-
-sig_cutoff = 0.05 / 6 # bonforroni corrected for all electrodes
-
-for electrode in range(0,6):
-    
-    print('\n' + electrode_names[electrode] + '\n')
-    
-    standing_self_correlations_hall = self_split_correlation[:, 0, electrode, 0] 
-    standing_self_correlations_lobby = self_split_correlation[:, 1, electrode, 0] 
-    mid_self_correlations_hall = self_split_correlation[:, 0, electrode, 2] 
-    mid_self_correlations_lobby = self_split_correlation[:, 1, electrode, 2] 
-    
-    # only use subjects for which the self correlation is above a certain threshold
-    subjects_to_use = []
-    for subject in range(0,25):
-        if standing_self_correlations_hall[subject] > cc and standing_self_correlations_lobby[subject] > cc and mid_self_correlations_hall[subject] > cc and mid_self_correlations_lobby[subject] > cc:
-            subjects_to_use.append(subject)
-    
-    if 3 in subjects_to_use: # subject 3 has big artefacts, don't use
-        subjects_to_use.remove(3)
-
-    standing_walking_phase_differences_electrode_hall = np.zeros([len(subjects_to_use,)])
-    standing_walking_phase_differences_electrode_lobby = np.zeros([len(subjects_to_use,)])
-
-    standing_walking_phase_differences_VEOG_hall = np.zeros([len(subjects_to_use,)])
-    standing_walking_phase_differences_VEOG_lobby = np.zeros([len(subjects_to_use,)])
-    
-    subject_count = 0
-
-    for subject in subjects_to_use:
-        
-        standing_SSVEP_hall = all_SSVEPs[subject, 0, electrode, 0,:]
-        mid_SSVEP_hall = all_SSVEPs[subject, 0, electrode, 2,:]
-
-        standing_SSVEP_lobby = all_SSVEPs[subject, 1, electrode, 0,:]
-        mid_SSVEP_lobby = all_SSVEPs[subject, 1, electrode, 2,:]
-        
-            
-        VEOG_standing_hall_SSVEP = all_SSVEPs[subject,0,7,0,:] # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
-        VEOG_walking_hall_SSVEP = all_SSVEPs[subject,0,7,1,:]
-        
-        VEOG_standing_lobby_SSVEP = all_SSVEPs[subject,1,7,0,:] # subject, location, electrode, condition, SSVEP data (25 data points = 40 Hz)
-        VEOG_walking_lobby_SSVEP = all_SSVEPs[subject,1,7,1,:]
-
-        
-        standing_walking_phase_differences_electrode_hall[subject_count] = functions.cross_correlation_absolute(standing_SSVEP_hall, mid_SSVEP_hall)
-        standing_walking_phase_differences_electrode_lobby[subject_count] = functions.cross_correlation_absolute(standing_SSVEP_lobby, mid_SSVEP_lobby)
-        
-        standing_walking_phase_differences_VEOG_hall[subject_count] = functions.cross_correlation_absolute(VEOG_standing_hall_SSVEP, VEOG_walking_hall_SSVEP)
-        standing_walking_phase_differences_VEOG_lobby[subject_count] = functions.cross_correlation_absolute(VEOG_standing_lobby_SSVEP, VEOG_walking_lobby_SSVEP)
-                
-        subject_count += 1
-        
-        
-        
-    Z_score_hall = functions.group_permutation_test(standing_walking_phase_differences_electrode_hall, standing_walking_phase_differences_VEOG_hall)
-    p_value_hall = scipy.stats.norm.sf(abs(Z_score_hall))
-
-    Z_score_lobby = functions.group_permutation_test(standing_walking_phase_differences_electrode_lobby, standing_walking_phase_differences_VEOG_lobby)
-    p_value_lobby = scipy.stats.norm.sf(abs(Z_score_lobby))
-
-    grand_average_phase_difference_standing_walking_electrode = (standing_walking_phase_differences_electrode_hall + standing_walking_phase_differences_electrode_lobby)/2
-    grand_average_phase_difference_standing_walking_VEOG  = (standing_walking_phase_differences_VEOG_hall + standing_walking_phase_differences_VEOG_lobby)/2
-    Z_score_grand_average = functions.group_permutation_test(grand_average_phase_difference_standing_walking_electrode, grand_average_phase_difference_standing_walking_VEOG)
-    p_value_grand_average = scipy.stats.norm.sf(abs(Z_score_grand_average))
-
-    print('Hall: Z = ' + str(Z_score_hall) + ' p = ' + str(p_value_hall))
-    if p_value_hall < sig_cutoff:
-        print('SIGNIFICANT')
-    print('Lobby: Z = ' + str(Z_score_lobby) + ' p = ' + str(p_value_lobby))
-    if p_value_lobby < sig_cutoff:
-        print('SIGNIFICANT')
-    print('Grand average Z score = ' + str(Z_score_grand_average) + ' p = ' + str(p_value_grand_average))
-    if p_value_grand_average < sig_cutoff:
-        print('SIGNIFICANT')
-
-
 
 
 
@@ -573,3 +494,17 @@ plt.subplot(1,2,2)
 x = np.arange(0,8)
 plt.xticks(x, electrode_names[0:8])    
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
