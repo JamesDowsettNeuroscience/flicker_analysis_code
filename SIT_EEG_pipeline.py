c0cded6ca916c6908b2b2614ccde9105470a35dd
James Dowsett <James.Dowsett@psy.lmu.de>
2023-01-30 18:26:53 +0100

    VOR pipeline update

--- SIT_EEG_pipeline.py
+++ SIT_EEG_pipeline.py
@@ -14,7 +14,7 @@ from flicker_analysis_package import functions
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+import scipy.signal
 
 path = '/home/james/Active_projects/SIT_EEG/SIT_pilot/'
 
@@ -30,131 +30,207 @@ period = 250 # 4 Hz sound
 
 plt.figure()
 
-for file_name in file_names:
+# for file_name in file_names:
 
-    
-    
-    ### load triggers
-    
-    walking_triggers_file_name = file_name + '_all_triggers.npy'
-    
-    all_triggers = np.load(path + walking_triggers_file_name)
-    
-    
-    ## sort triggers
-    
-    diff_triggers = np.diff(all_triggers)
-    
-    # empty lists to put the triggers into for each condition
-    search_triggers_list = []
-    target_triggers_list = []
-    
-    for trig_count in range(0,len(diff_triggers)):
-        
-        # if trigger is approx. 250 ms before the next trigger then this is a search trigger
-        if diff_triggers[trig_count] <= 252 and diff_triggers[trig_count] >= 248:
-            
-            search_triggers_list.append(all_triggers[trig_count])
-            
-        # if trigger is a target trigger (target triggers come in pairs, only use the first which has approx. 121 ms time difference)
-        if diff_triggers[trig_count] <= 123 and diff_triggers[trig_count] >= 117:
-            
-            ## check trigger is not part of demonstration sounds which come at the begining of each trial
-            
-            next_6_triggers = diff_triggers[trig_count:trig_count+6]
-            
-            if max(next_6_triggers) < 132 and min(next_6_triggers) > 117: # must be 6 consecutive triggers in correct time range
-            
-                target_triggers_list.append(all_triggers[trig_count])
-            
+file_name = file_names[0]
+ 
+ ### load triggers
+ 
+walking_triggers_file_name = file_name + '_all_triggers.npy'
+
+all_triggers = np.load(path + walking_triggers_file_name)
+
+
+## sort triggers
+
+diff_triggers = np.diff(all_triggers)
+
+# empty lists to put the triggers into for each condition
+search_triggers_list = []
+target_triggers_list = []
+
+for trig_count in range(0,len(diff_triggers)):
+
+    # if trigger is approx. 250 ms before the next trigger then this is a search trigger
+    if diff_triggers[trig_count] <= 252 and diff_triggers[trig_count] >= 248:
+        search_triggers_list.append(all_triggers[trig_count])
+         
+     # if trigger is a target trigger (target triggers come in pairs, only use the first which has approx. 121 ms time difference)
+    if diff_triggers[trig_count] <= 123 and diff_triggers[trig_count] >= 117:
+         
+         ## check trigger is not part of demonstration sounds which come at the begining of each trial
+         
+         next_6_triggers = diff_triggers[trig_count:trig_count+6]
+         
+         if max(next_6_triggers) < 132 and min(next_6_triggers) > 117: # must be 6 consecutive triggers in correct time range
+         
+             target_triggers_list.append(all_triggers[trig_count])
+         
+ 
+# convert to numpy arrays
+search_triggers = np.array(search_triggers_list, dtype=int)
+target_triggers = np.array(target_triggers_list, dtype=int)
+
+print('\n' + file_name + '  ' + str(len(search_triggers)) + ' search triggers')
+print('\n' + file_name + '  ' + str(len(target_triggers)) + ' target triggers')  
+
+## make triggere time series for plotting
+
+# all_triggers_time_series = np.zeros([max(all_triggers)+1, ])
+# search_triggers_time_series = np.zeros([max(all_triggers)+1, ])
+# target_triggers_time_series = np.zeros([max(all_triggers)+1, ])
+
+
+# for trigger in all_triggers:
+#     all_triggers_time_series[trigger] = 10
+
+# for trigger in search_triggers:
+#     search_triggers_time_series[trigger] = 5
+
+# for trigger in target_triggers:
+#     target_triggers_time_series[trigger] = 5
+
+# plt.plot(all_triggers_time_series)
+# plt.plot(search_triggers_time_series)
+# plt.plot(target_triggers_time_series)
+
+
+## get REF2 data
+data_file_name = file_name + '_chan_7_data.npy'
+
+REF2_data = np.load(path + data_file_name)         
+
+ 
+ 
+ ### loop for each electrode
+ 
+#plt.figure()
+# plt.suptitle(file_name)
+ 
+plot_count = 0
+
+ #for electrode in (2,5,6):
+electrode = 2
+
+data_file_name = file_name + '_chan_' + str(electrode) +  '_data.npy'
+
+data = np.load(path + data_file_name)         
+
+# re-reference
+data = data - (REF2_data/2)
+
+ ## filter
+cutoff = 40 # cutoff in Hz
+nyquist = 1000/2
+Wn = cutoff/nyquist
+b, a = scipy.signal.butter(3, Wn, 'low')
+data = scipy.signal.filtfilt(b, a, data)
+
+ 
+ 
+# make SSVEP
+search_SSVEP = functions.make_SSVEPs(data, search_triggers, period) #
+
+target_SSVEP = functions.make_SSVEPs(data, target_triggers, period) # 
+
+
+
+
+## baseline correct
+search_SSVEP = search_SSVEP - search_SSVEP.mean()
+target_SSVEP = target_SSVEP - target_SSVEP.mean()
+
+
+
+
+
+## calculate std. deviation values
+
+search_seg_matrix = np.zeros([len(search_triggers),period]) 
+trig_count = 0
+for trig in search_triggers:
+    search_seg_matrix[trig_count,:] = data[trig:trig+period]
+    trig_count += 1
+
+search_std = np.zeros([period,])
+for t in range(0,period):
+    search_std[t] = np.std(search_seg_matrix[:,t])
+
+target_seg_matrix = np.zeros([len(target_triggers),period]) 
+trig_count = 0
+for trig in target_triggers:
+    target_seg_matrix[trig_count,:] = data[trig:trig+period]
+    trig_count += 1
+
+target_std = np.zeros([period,])
+for t in range(0,period):
+    target_std[t] = np.std(target_seg_matrix[:,t])
+
+
+# calculate std error
+
+search_std_error = search_std/np.sqrt(len(search_triggers))
+target_std_error = target_std/np.sqrt(len(target_triggers))
+
+search_std_error = search_std_error /20
+target_std_error  = target_std_error /20
+
+ ## convert to micro- volts
+ 
+search_SSVEP = search_SSVEP * 1000000
+target_SSVEP = target_SSVEP * 1000000
+
+search_std_error = search_std_error * 1000000
+target_std_error = target_std_error * 1000000
+
+plot_count += 1
+#plt.subplot(1,3,plot_count)
+
+plt.title(electrode_names[electrode])
+ 
+# if file_name == 'AKG_overear':
+#      plt.plot(search_SSVEP, 'b', label = 'Search Overear')
+#      plt.plot(target_SSVEP, 'r', label = 'Target Overear')
+# elif file_name == 'ear_buds':
+#      plt.plot(search_SSVEP, 'c', label = 'Search Ear buds')
+#      plt.plot(target_SSVEP, 'm', label = 'Target Ear buds')        
      
-    # convert to numpy arrays
-    search_triggers = np.array(search_triggers_list, dtype=int)
-    target_triggers = np.array(target_triggers_list, dtype=int)
+plt.plot(search_SSVEP, 'b', label = 'Search')
+plt.plot(target_SSVEP, 'r', label = 'Target')
     
-    print('\n' + file_name + '  ' + str(len(search_triggers)) + ' search triggers')
-    print('\n' + file_name + '  ' + str(len(target_triggers)) + ' target triggers')  
-    
-    ## make triggere time series for plotting
-    
-    # all_triggers_time_series = np.zeros([max(all_triggers)+1, ])
-    # search_triggers_time_series = np.zeros([max(all_triggers)+1, ])
-    # target_triggers_time_series = np.zeros([max(all_triggers)+1, ])
-    
-    
-    # for trigger in all_triggers:
-    #     all_triggers_time_series[trigger] = 10
-    
-    # for trigger in search_triggers:
-    #     search_triggers_time_series[trigger] = 5
-    
-    # for trigger in target_triggers:
-    #     target_triggers_time_series[trigger] = 5
-    
-    # plt.plot(all_triggers_time_series)
-    # plt.plot(search_triggers_time_series)
-    # plt.plot(target_triggers_time_series)
-    
-    
-    ## get REF2 data
-    data_file_name = file_name + '_chan_7_data.npy'
-    
-    REF2_data = np.load(path + data_file_name)         
-    
-    
-    
-    ### loop for each electrode
-    
-    #plt.figure()
-   # plt.suptitle(file_name)
-    
-    plot_count = 0
-    
-    #for electrode in (2,5,6):
-    electrode = 2
-        
-    data_file_name = file_name + '_chan_' + str(electrode) +  '_data.npy'
-    
-    data = np.load(path + data_file_name)         
-    
-    # re-reference
-    data = data - (REF2_data/2)
-    
-    # make SSVEP
-    search_SSVEP = functions.make_SSVEPs(data, search_triggers, period) # SIGI was always 40 Hz, length = 25
-    
-    target_SSVEP = functions.make_SSVEPs(data, target_triggers, period) # SIGI was always 40 Hz, length = 25
-    
-    
-    ## baseline correct
-    search_SSVEP = search_SSVEP - search_SSVEP.mean()
-    target_SSVEP = target_SSVEP - target_SSVEP.mean()
-    
-    ## convert to micro- volts
-    
-    search_SSVEP = search_SSVEP * 1000000
-    target_SSVEP = target_SSVEP * 1000000
-    
-    plot_count += 1
-    #plt.subplot(1,3,plot_count)
-    
-    plt.title(electrode_names[electrode])
-    
-    if file_name == 'AKG_overear':
-        plt.plot(search_SSVEP, 'b', label = 'Search Overear')
-        plt.plot(target_SSVEP, 'r', label = 'Target Overear')
-    elif file_name == 'ear_buds':
-        plt.plot(search_SSVEP, 'c', label = 'Search Ear buds')
-        plt.plot(target_SSVEP, 'm', label = 'Target Ear buds')        
-    
-    
-    # plt.legend()
+ 
+ # plt.legend()
 
-    plt.ylim([-2.5, 4.5])
-    
-    
-    plt.xlabel('Time (ms)')
-    
-    plt.ylabel('Voltage (\u03BCV)')
-    
+# plt.ylim([-2.5, 4.5])
+# plt.ylim([-2.5, 2.5])
+
+
+plt.xlabel('Time (ms)')
+
+plt.ylabel('Voltage (\u03BCV)')
+
 plt.legend()
+
+
+
+
+from matplotlib import pyplot as plt
+
+
+x = np.linspace(0, period, period)
+
+plt.fill_between(x, search_SSVEP-search_std_error, search_SSVEP+search_std_error,
+                 alpha=0.2, edgecolor='b', facecolor='c',
+                 linewidth=1, linestyle='solid', antialiased=True)
+
+plt.fill_between(x, target_SSVEP-target_std_error, target_SSVEP+target_std_error,
+                 alpha=0.2, edgecolor='r', facecolor='m',
+                 linewidth=1, linestyle='solid', antialiased=True)
+
+
+
+
+plt.show()
+
+
+
--- VOR_smooth_pursuit_pipeline.py
+++ VOR_smooth_pursuit_pipeline.py
@@ -8,6 +8,8 @@ Created on Fri Jan 27 12:38:48 2023
 
 ############ analysis pipeline for VOR-smooth pursuit ficker experiment ##################
 
+from flicker_analysis_package import functions
+
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,7 +28,7 @@ frequency_names = ('10 Hz', '40 Hz')
 
 sample_rate = 5000
 
-num_subjects = 2
+num_subjects = 3
 
 period_10Hz = 500 # period of flicker in data points
 period_40Hz = 125 # period of flicker in data points
@@ -34,130 +36,197 @@ period_40Hz = 125 # period of flicker in data points
 length = 1 # length of FFT in seconds
 
 ## trigger times for linear interpolation
-trig_1_time = -3
-trig_2_time = 48
-trig_length = 6
+trig_1_time_10Hz = -1
+trig_2_time_10Hz = 248
+trig_length_10Hz = 20
 
+trig_1_time_40Hz = -1
+trig_2_time_40Hz = 59
+trig_length_40Hz = 20
 
 
 #######################################
 
 
-subject = 1
-
-frequency = 0 # 0 = 10Hz, 1 = 40Hz
-
-for condition_count in range(0,6):
+for subject in range(1,4):
     
-    if frequency == 0:
-        condition = condition_count+1
-    elif frequency == 1:
-        condition = condition_count+7 
+    print('\n Subject ' + str(subject) + '\n')
+    
+    plt.figure(subject)
+    plt.suptitle('Subject ' + str(subject))
+    
+    for frequency in (0,1): # 0 = 10Hz, 1 = 40Hz
         
-    print(condition_names[condition_count])
-    
-
-    ## load all data
-    all_data = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_all_data.npy')
-    
-    ## get EOG data
-    HEOG_data = all_data[31,:]
-    VEOG_data = all_data[30,:]
-
-    HEOG_data = HEOG_data - HEOG_data.mean() # baseline correct
-    VEOG_data = VEOG_data - VEOG_data.mean()
-    
-    
-    ## filter EOG data
-    
-    nyquist = sample_rate/2
-
-    # low pass HEOG
-    cutoff = 1 # cutoff in Hz
-    Wn = cutoff/nyquist
-    b, a = scipy.signal.butter(3, Wn, 'low')
-    HEOG_data = scipy.signal.filtfilt(b, a, HEOG_data)
-    
-    # high pass HEOG
-    cutoff = 0.05 # cutoff in Hz
-    Wn = cutoff/nyquist
-    b, a = scipy.signal.butter(3, Wn, 'high')
-    HEOG_data = scipy.signal.filtfilt(b, a, HEOG_data)
-
-    # low pass VEOG
-    cutoff = 5 # cutoff in Hz
-    Wn = cutoff/nyquist
-    b, a = scipy.signal.butter(3, Wn, 'low')
-    VEOG_data = scipy.signal.filtfilt(b, a, VEOG_data)
-    
-    # high pass VEOG
-    cutoff = 1 # cutoff in Hz
-    Wn = cutoff/nyquist
-    b, a = scipy.signal.butter(3, Wn, 'high')
-    VEOG_data = scipy.signal.filtfilt(b, a, VEOG_data)
-
-    
-
-    ## load triggers
-    all_flicker_triggers = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_flicker_triggers.npy')
-    
-    all_movement_triggers = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_movement_triggers.npy')
-    
-    # make trigger time series
-    flicker_trigger_time_series = np.zeros([all_flicker_triggers[-1]+1,])
-    movement_trigger_time_series = np.zeros([all_movement_triggers[-1]+1,])
-    
-    for trigger in all_flicker_triggers:
-        flicker_trigger_time_series[trigger] = 0.0001
-        
-    for trigger in all_movement_triggers:
-        movement_trigger_time_series[trigger] = 0.0002
-       
-    plt.figure()
-    plt.title(condition_names[condition-1])
-    plt.plot(flicker_trigger_time_series)
-    plt.plot(movement_trigger_time_series)
-    
-    plt.plot(HEOG_data)
-    plt.plot(VEOG_data)
-    
-    
-    ## sort triggers 
-    good_flicker_triggers_list = []
-    
-    
-    wait_time = 5000 # one second wait time for the moving condition
-
-    for trigger in all_flicker_triggers:
-        
-        trigger_distance_from_movement = all_movement_triggers - trigger
-        
-        # select the next movement trigger in the future
-        nearest_movement_trigger = min(trigger_distance_from_movement[trigger_distance_from_movement>0])
-        
-        # if next movement trigger is more than one second away
-        if nearest_movement_trigger > wait_time: 
+        for condition_count in range(0,6):
             
-            # check for eye blinks
-            VEOG_segment = VEOG_data[trigger-500:trigger+500]
+            if frequency == 0:
+                condition = condition_count+1
+                period = period_10Hz
+            elif frequency == 1:
+                condition = condition_count+7 
+                period = period_40Hz
+                
+            print(condition_names[condition_count])
             
-            if np.ptp(VEOG_segment) < 0.00005:
+        
+            ## load all data
+            all_data = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_all_data.npy')
             
-                good_flicker_triggers_list.append(trigger)
+            ## get EOG data
+            HEOG_data = all_data[31,:]
+            VEOG_data = all_data[30,:]
+        
+            HEOG_data = HEOG_data - HEOG_data.mean() # baseline correct
+            VEOG_data = VEOG_data - VEOG_data.mean()
+            
+            
+            ## filter EOG data
+            
+            nyquist = sample_rate/2
+        
+            
+            # low pass HEOG
+            cutoff = 1 # cutoff in Hz
+            Wn = cutoff/nyquist
+            b, a = scipy.signal.butter(3, Wn, 'low')
+            HEOG_data = scipy.signal.filtfilt(b, a, HEOG_data)
+           
+            if condition_count < 3: # only high pass filter the eyes moving conditions 
+                # high pass HEOG
+                cutoff = 0.05 # cutoff in Hz
+                Wn = cutoff/nyquist
+                b, a = scipy.signal.butter(3, Wn, 'high')
+                HEOG_data = scipy.signal.filtfilt(b, a, HEOG_data)
         
         
         
+            # low pass VEOG
+            cutoff = 5 # cutoff in Hz
+            Wn = cutoff/nyquist
+            b, a = scipy.signal.butter(3, Wn, 'low')
+            VEOG_data = scipy.signal.filtfilt(b, a, VEOG_data)
+            
+            # high pass VEOG
+            cutoff = 1 # cutoff in Hz
+            Wn = cutoff/nyquist
+            b, a = scipy.signal.butter(3, Wn, 'high')
+            VEOG_data = scipy.signal.filtfilt(b, a, VEOG_data)
         
-    # plot time series of good triggers   
-    good_flicker_triggers_time_series = np.zeros([all_flicker_triggers[-1]+1,])
-    for trigger in good_flicker_triggers_list:
-        good_flicker_triggers_time_series[trigger] = 0.00015
-     
+            
         
-    plt.plot(good_flicker_triggers_time_series)
-    
-    
-    
-    
-    
-    
+            ## load triggers
+            all_flicker_triggers = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_flicker_triggers.npy')
+            
+            all_movement_triggers = np.load(path + 'S' + str(subject) + '_' + str(condition) + '_movement_triggers.npy')
+            
+            # make trigger time series
+            flicker_trigger_time_series = np.zeros([all_flicker_triggers[-1]+1,])
+            movement_trigger_time_series = np.zeros([all_movement_triggers[-1]+1,])
+            
+            for trigger in all_flicker_triggers:
+                flicker_trigger_time_series[trigger] = 0.0001
+                
+            for trigger in all_movement_triggers:
+                movement_trigger_time_series[trigger] = 0.0002
+               
+            # plt.figure()
+            # plt.title(condition_names[condition-1])
+            # plt.plot(flicker_trigger_time_series)
+            # plt.plot(movement_trigger_time_series)
+            
+        
+            
+            ## sort triggers 
+            good_flicker_triggers_list = []
+            
+            
+            wait_time = 5000 # one second wait time for the moving condition
+        
+            for trigger in all_flicker_triggers:
+                
+                trigger_distance_from_movement = all_movement_triggers - trigger
+                
+                # for the movement condition, the trigger marks the start of the movement, preceded by 0ne second of still
+                if condition_count < 3: 
+                    
+                    # select the next movement trigger in the future
+                    if len(trigger_distance_from_movement[trigger_distance_from_movement>0]) > 0:
+                        nearest_movement_trigger = min(trigger_distance_from_movement[trigger_distance_from_movement>0])
+                        
+                        # if next movement trigger is more than one second away
+                        if nearest_movement_trigger > wait_time: 
+                            
+                            # check for eye blinks
+                            VEOG_segment = VEOG_data[trigger-500:trigger+500]
+                            
+                            if np.ptp(VEOG_segment) < 0.00005:
+                            
+                                good_flicker_triggers_list.append(trigger)
+                                
+                                
+                   # for the fixation conditions, the trigger marks start of approx. 1 second of movement         
+                elif condition_count >=3: 
+                    
+                    # select the next movement trigger in the past
+                    if len(trigger_distance_from_movement[trigger_distance_from_movement<0]) > 0:
+                       
+                        nearest_movement_trigger = max(trigger_distance_from_movement[trigger_distance_from_movement<0])
+                    
+                        if np.abs(nearest_movement_trigger) > 5000: # ignore triggers 1 second after movement trigger
+                        
+                            # check for eye blinks
+                            VEOG_segment = VEOG_data[trigger-500:trigger+500]
+                            
+                            if np.ptp(VEOG_segment) < 0.00005:
+                            
+                                good_flicker_triggers_list.append(trigger)
+                    
+                    
+                
+                
+            # plot time series of good triggers   
+            good_flicker_triggers_time_series = np.zeros([all_flicker_triggers[-1]+1,])
+            for trigger in good_flicker_triggers_list:
+                good_flicker_triggers_time_series[trigger] = 0.00015
+             
+                
+            # plt.plot(good_flicker_triggers_time_series)
+            
+            # plt.plot(HEOG_data)
+            # plt.plot(VEOG_data)
+            
+            plt_count = 0
+            for electrode in (6,7):
+                
+                plt_count += 1
+                
+                data = all_data[electrode,:]
+        
+                # ### linear interpolation
+                if frequency == 0:
+                    trig_1_time = trig_1_time_10Hz
+                    trig_2_time = trig_2_time_10Hz
+                    trig_length =  trig_length_10Hz
+                elif frequency == 1:
+                    trig_1_time = trig_1_time_40Hz
+                    trig_2_time = trig_2_time_40Hz
+                    trig_length =  trig_length_40Hz
+
+                
+                data_linear_interpolation = functions.linear_interpolation(data, good_flicker_triggers_list, trig_1_time, trig_2_time, trig_length)
+                
+        
+                # make SSVEP
+                SSVEP = functions.make_SSVEPs(data_linear_interpolation, good_flicker_triggers_list, period) # SIGI was always 40 Hz, length = 25
+        
+        
+                plt.subplot(2,2,plt_count+(frequency*2))
+                
+                plt.plot(SSVEP, label = condition_names[condition_count])
+                
+                plt.title(electrode_names[electrode] + '  ' + frequency_names[frequency])
+                
+                
+                
+                
+    plt.legend()
