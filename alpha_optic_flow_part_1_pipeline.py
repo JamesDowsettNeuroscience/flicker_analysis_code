7d82afad488728ae779bc60e7550e051919cd1c2
James Dowsett <James.Dowsett@psy.lmu.de>
2023-03-29 18:30:40 +0200

    update alpha flow and added decode 3 way function

--- alpha_optic_flow_part_1_pipeline.py
+++ alpha_optic_flow_part_1_pipeline.py
@@ -39,15 +39,16 @@ freq_bins = np.arange(72,125,5) # make frequency bins
 
 all_SSVEPs = np.zeros([num_subjects, 6, 3, 2, len(freq_bins), max(freq_bins)]) # subject, electrode, condition, block, freq bins, SSVEP data
 
+all_SSVEP_amplitudes = np.zeros([num_subjects, 6, 3, 2, len(freq_bins)])
+
 trials_with_no_triggers = []
 trials_with_low_trigger_count = []
 
-
-subjects_to_use = (1,2,3,5,6,7,8,9,10,11,12,13,15,16,17,19,20)
+subject_numbers_with_low_trigger_counts = []
 
 subject_count = 0
 
-for subject in subjects_to_use:
+for subject in range(1,21):
     
     print('\n Subject ' + str(subject) + '\n')
     
@@ -136,11 +137,14 @@ for subject in subjects_to_use:
                            # trigger_time_series[good_triggers[trig_count],current_bin] = 1
                             
                 num_triggers_for_bin = len(triggers_for_this_bin)
+                
                 print(str(num_triggers_for_bin) + ' segments ' + str(period) + ' ms = ' + str(np.round(sample_rate/freq_bins[current_bin],2)) + ' Hz')
                 
                 numbers_of_triggers_per_bin[current_bin] = num_triggers_for_bin
                 
-                
+                ## save the triggers
+                triggers_for_this_bin_array = np.asarray(triggers_for_this_bin)
+                np.save(path + trial_name + '_bin_' + str(current_bin), triggers_for_this_bin_array)
                 
              #   plt.suptitle(str(num_triggers_for_bin) + ' segments ' + str(period) + ' ms = ' + str(np.round(sample_rate/freq_bins[current_bin],2)) + ' Hz')
                 
@@ -161,6 +165,7 @@ for subject in subjects_to_use:
                     # subject, electrode, condition, block, freq bins, SSVEP data
                     all_SSVEPs[subject_count,electrode, condition-1, block-1, current_bin, 0:period] = SSVEP
         
+                    all_SSVEP_amplitudes[subject_count,electrode, condition-1, block-1, current_bin] = np.ptp(SSVEP)
                     
                     #plt.plot(data, label = electrode_names[electrode])
                   #  plt.plot(SSVEP, label = block)
@@ -170,22 +175,31 @@ for subject in subjects_to_use:
                 
             if min(numbers_of_triggers_per_bin) < 50:
                 trials_with_low_trigger_count.append(trial_name)
-                
+                subject_numbers_with_low_trigger_counts.append(subject_count)
                 
                 
     subject_count += 1
-                
-                
-              
- 
+       
+         
+ # remove subjects with low trigger counts. These subjects can be ignored             
+# remove duplicates             
+subject_numbers_with_low_trigger_counts = list(dict.fromkeys(subject_numbers_with_low_trigger_counts)) 
+
+subjects_to_use = list(range(20))    
+
+for bad_subject in subject_numbers_with_low_trigger_counts:
+    subjects_to_use.remove(bad_subject)
+
+
+
+
 
 
 
 ##########   get individual alpha frequencies   ###############################
 
-subjects_to_use = np.arange(1,21)
 
-electrode = 0
+electrode = 3
 
 length = 5 # length of FFT in seconds
 
@@ -200,20 +214,20 @@ condition_colours = ('b', 'r', 'g')
 
 subject_count = 0
 
-alpha_peaks = np.zeros([len(subjects_to_use), 3])
-IAFs = np.zeros([len(subjects_to_use), 3])
+alpha_peaks = np.zeros([num_subjects, 3])
+IAFs = np.zeros([num_subjects, 3])
 
+all_induced_FFTs = np.zeros([num_subjects, 3, length*sample_rate])
 
 subject_count = 0
 
-for subject in subjects_to_use:
+for subject in range(1,21):
     
     print('\n Subject ' + str(subject) + '\n')
       
     plt.subplot(4,5,subject)
-   
-    
-    
+
+
     for condition in(1,2,3):
         
         print('Condition ' + str(condition))
@@ -234,6 +248,8 @@ for subject in subjects_to_use:
             
         induced_fft = functions.induced_fft(data, length, sample_rate)
         
+        all_induced_FFTs[subject_count, condition-1,:] = induced_fft
+        
         plt.plot(freq_axis, induced_fft, color = condition_colours[condition-1])
         
         alpha_range = induced_fft[8*length:14*length]
@@ -253,20 +269,52 @@ for subject in subjects_to_use:
                   
     subject_count += 1
                   
-                    
-                  
-             
-                
-             
-                
-             
-                
+                 
 
-###### check block 1 and 2 correlations ##############
+##### compare SSVEP amplitudes with resting FFT  ########
 
-# block_1_and_2_correlations = np.zeros([subject_count,6,3,len(freq_bins)])
 
-# for subject in range(0,subject_count):
+flicker_frequencies_per_bin = np.round(1000/freq_bins,1)
+
+# for electrode in range(0,6):
+    
+#     plt.subplot(3,2,electrode+1)
+#     plt.title(electrode_names[electrode])
+        
+        
+#     for condition in range(0,3):
+        
+#         # subject, electrode, condition, block, freq bins
+#         block_1_SSVEP_amplitudes = all_SSVEP_amplitudes[subjects_to_use,electrode,condition, 0,:]
+#         block_2_SSVEP_amplitudes = all_SSVEP_amplitudes[subjects_to_use,electrode,condition, 1,:]
+    
+#         average_across_participants_block_1 = np.nanmean(block_1_SSVEP_amplitudes, axis=0)
+#         average_across_participants_block_2 = np.nanmean(block_2_SSVEP_amplitudes, axis=0)
+                 
+#         average_across_blocks = (average_across_participants_block_1 + average_across_participants_block_2) / 2
+        
+                 
+#         plt.plot(flicker_frequencies_per_bin,average_across_blocks*350, color = condition_colours[condition])
+                
+#         all_subjects_FFTs = all_induced_FFTs[subjects_to_use, condition,:]
+#         average_FFT = all_subjects_FFTs.mean(axis=0)
+                
+#         plt.plot(freq_axis,average_FFT, color = condition_colours[condition])
+    
+#         plt.xlim([4, 16])
+#         plt.ylim([0, 0.0025])
+
+
+
+
+
+
+# ###### check block 1 and 2 correlations ##############
+
+# block_1_and_2_correlations = np.zeros([len(subjects_to_use),6,3,len(freq_bins)])
+
+# subject_count = 0
+# for subject in subjects_to_use:
     
 #     for electrode in range(0,6):
     
@@ -280,8 +328,9 @@ for subject in subjects_to_use:
 #                 block_1_SSVEP = all_SSVEPs[subject, electrode, condition, 0, current_bin, 0:period]
 #                 block_2_SSVEP = all_SSVEPs[subject, electrode, condition, 1, current_bin, 0:period]
                 
-#                 block_1_and_2_correlations[subject, electrode, condition, current_bin] = np.corrcoef(block_1_SSVEP,block_2_SSVEP)[0,1]
+#                 block_1_and_2_correlations[subject_count, electrode, condition, current_bin] = np.corrcoef(block_1_SSVEP,block_2_SSVEP)[0,1]
     
+#     subject_count += 1
     
 # # plot block 1 and 2 correlations
 # plt.figure()
@@ -309,13 +358,16 @@ for subject in subjects_to_use:
 
 
 
-######### plot P3/P4 ratios ############
+# ######### plot P3/P4 ratios ############
 
-# P3_P4_ratios = np.zeros([subject_count,3, len(freq_bins)])
+
+# P3_P4_ratios = np.zeros([len(subjects_to_use),3, len(freq_bins)])
 
 # plot_colors = ('b', 'r', 'g')
 
-# for subject in range(0,subject_count):
+# subject_count = 0
+
+# for subject in subjects_to_use:
     
 #     for condition in range(0,3):
 
@@ -345,12 +397,13 @@ for subject in subjects_to_use:
            
 #             P3_P4_ratio = np.ptp(SSVEP_P3) / np.ptp(SSVEP_P4)
             
-#             P3_P4_ratios[subject, condition, current_bin] = P3_P4_ratio
+#             P3_P4_ratios[subject_count, condition, current_bin] = P3_P4_ratio
             
-#            # print(P3_P4_ratio)
+#             # print(P3_P4_ratio)
+#     subject_count += 1
             
             
-# average_P3_P4_ratios = P3_P4_ratios.mean(axis=0)
+# average_P3_P4_ratios = P3_P4_ratios[:,:,:].mean(axis=0)
 
 
 
@@ -361,7 +414,7 @@ for subject in subjects_to_use:
     
 #     plt.plot(1000/freq_bins,average_P3_P4_ratios[condition,:], label = condition_names[condition], color = plot_colors[condition])
 
-#    # for current_bin in range(0,len(freq_bins)):
+#     # for current_bin in range(0,len(freq_bins)):
         
 #       #  plt.scatter(np.ones([subject_count,])*(1000/freq_bins[current_bin])+(condition*0.05),P3_P4_ratios[:,condition,current_bin], s=1, c=plot_colors[condition])
 
@@ -370,6 +423,8 @@ for subject in subjects_to_use:
 
 # plt.plot(1000/freq_bins,np.ones(len(freq_bins)), 'k--')
 
+# plt.xlabel('Flicker Frequency (Hz)')
+# plt.ylabel('P3/P4 Ratio')
 
 # print('\nFrequency bins = ')
 # print(1000/freq_bins)
@@ -429,3 +484,106 @@ for subject in subjects_to_use:
 #             plt.ylim([0, 2000])
             
             
+            
+
+
+
+################ Decoding #####################
+
+for electrode in range(0,6):
+    
+    decoding_accuracy = np.zeros([num_subjects,6,2, len(freq_bins)])
+      
+    subject_count = 0
+      
+    for subject in range(1,21):
+        
+        print('\n Subject ' + str(subject) + '\n')
+        
+    
+        for block in(1,2):
+            
+            print('\n Block ' + str(block) + '\n')
+            
+        
+            trial_name = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_1' 
+            all_data = np.load(path + trial_name + '_all_data.npy')
+            data_1 = all_data[electrode,:]
+            
+            trial_name = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_2' 
+            all_data = np.load(path + trial_name + '_all_data.npy')
+            data_2 = all_data[electrode,:]
+            
+            trial_name = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_3' 
+            all_data = np.load(path + trial_name + '_all_data.npy')
+            data_3 = all_data[electrode,:]
+            
+            
+            for current_bin in range(0,len(freq_bins)):
+                
+                trial_name_1 = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_1' 
+                triggers_1 = np.load(path + trial_name_1 + '_bin_' + str(current_bin) + '.npy')
+            
+                trial_name_2 = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_2' 
+                triggers_2 = np.load(path + trial_name_2 + '_bin_' + str(current_bin) + '.npy')
+                
+                trial_name_3 = 'subject_' + str(subject) + '_block_' + str(block) + '_condition_3' 
+                triggers_3 = np.load(path + trial_name_3 + '_bin_' + str(current_bin) + '.npy')
+    
+    
+                SSVEP_1 = functions.make_SSVEPs(data_1, triggers_1, freq_bins[current_bin])
+                SSVEP_2 = functions.make_SSVEPs(data_2, triggers_2, freq_bins[current_bin])
+                SSVEP_3 = functions.make_SSVEPs(data_3, triggers_3, freq_bins[current_bin])
+                
+                period = freq_bins[current_bin]
+                num_loops = 10
+                
+                average_percent_correct = functions.decode_correlation_3way(data_1, data_2, data_3, triggers_1, triggers_2, triggers_3, period, num_loops)
+                
+                decoding_accuracy[subject_count,electrode,block-1,current_bin] = average_percent_correct
+                
+                print('Average percent correct = ' + str(average_percent_correct))
+                
+                
+        subject_count += 1            
+                
+            
+
+            
+# for subject in range(0,20):
+    
+#     re_scale = alpha_peaks[subject,2] / 100 # re-scale the plot so 100% is the same as condition 3 peak
+    
+#     decoding_scores_for_block_1 = decoding_accuracy[subject,electrode,0,:] * re_scale
+#     decoding_scores_for_block_2 = decoding_accuracy[subject,electrode,1,:] * re_scale
+    
+#     average_decoding_scores = (decoding_scores_for_block_1 + decoding_scores_for_block_2) /2
+    
+#     plt.subplot(4,5,subject+1)
+#     plt.title('Subject ' + str(subject+1))       
+    
+#     # plt.plot(flicker_frequencies_per_bin, decoding_scores_for_block_1)
+#     # plt.plot(flicker_frequencies_per_bin, decoding_scores_for_block_2)
+    
+#     plt.plot(flicker_frequencies_per_bin, average_decoding_scores,'k')
+
+        
+## plot grand average
+    
+    decoding_scores_for_block_1 = decoding_accuracy[:,electrode,0,:]
+    average_decoding_scores_block_1 = decoding_scores_for_block_1.mean(axis=0)
+    
+    #plt.plot(flicker_frequencies_per_bin, average_decoding_scores_block_1)
+        
+    decoding_scores_for_block_2 = decoding_accuracy[:,electrode,1,:]
+    average_decoding_scores_block_2 = decoding_scores_for_block_2.mean(axis=0)
+    
+    #plt.plot(flicker_frequencies_per_bin, average_decoding_scores_block_2)
+        
+    
+    grand_average_decoding_scores = (average_decoding_scores_block_1 + average_decoding_scores_block_2) /2
+
+    plt.plot(flicker_frequencies_per_bin, grand_average_decoding_scores, label = electrode_names[electrode])
+    
+
+plt.legend()
